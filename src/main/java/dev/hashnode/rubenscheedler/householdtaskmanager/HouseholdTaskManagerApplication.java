355d869dc4439e8402982d724418c2db9674a52f
ruben <scheedler.r@tkppensioen.nl>
2023-09-27 15:17:32 +0200

    Add Spring initializr sources

---
+++ src/main/java/dev/hashnode/rubenscheedler/householdtaskmanager/HouseholdTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.hashnode.rubenscheedler.householdtaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hashnode/rubenscheedler/householdtaskmanager/HouseholdTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hashnode.rubenscheedler.householdtaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
