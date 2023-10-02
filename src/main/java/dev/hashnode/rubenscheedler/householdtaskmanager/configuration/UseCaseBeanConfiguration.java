980a5fe7a4597a0d36e006a11a024970b4340b0d
ruben <scheedler.r@tkppensioen.nl>
2023-10-02 15:17:08 +0200

    Add configuration for use case beans

---
+++ src/main/java/dev/hashnode/rubenscheedler/householdtaskmanager/configuration/UseCaseBeanConfiguration.java
@@ -0,0 +1,50 @@
+package dev.hashnode.rubenscheedler.householdtaskmanager.configuration;
+
+import dev.hashnode.rubenscheedler.householdtaskmanager.domain.port.input.*;
+import dev.hashnode.rubenscheedler.householdtaskmanager.domain.port.output.DeleteTaskPort;
+import dev.hashnode.rubenscheedler.householdtaskmanager.domain.port.output.GetTaskPort;
+import dev.hashnode.rubenscheedler.householdtaskmanager.domain.port.output.GetTasksPort;
+import dev.hashnode.rubenscheedler.householdtaskmanager.domain.port.output.SaveTaskPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class that is responsible for making all use case beans.
+ */
+@Configuration
+public class UseCaseBeanConfiguration {
+    @Bean
+    AssignTaskUseCase assignTaskUseCase(GetTaskPort getTaskPort, SaveTaskPort saveTaskPort) {
+        return new AssignTaskUseCase(getTaskPort, saveTaskPort);
+    }
+
+    @Bean
+    CompleteTaskUseCase completeTaskUseCase(GetTaskPort getTaskPort, SaveTaskPort saveTaskPort) {
+        return new CompleteTaskUseCase(getTaskPort, saveTaskPort);
+    }
+
+    @Bean
+    CreateTaskUseCase createTaskUseCase(SaveTaskPort saveTaskPort) {
+        return new CreateTaskUseCase(saveTaskPort);
+    }
+
+    @Bean
+    DeleteTaskUseCase deleteTaskUseCase(DeleteTaskPort deleteTaskPort) {
+        return new DeleteTaskUseCase(deleteTaskPort);
+    }
+
+    @Bean
+    EditTaskDescriptionUseCase editTaskDescriptionUseCase(GetTaskPort getTaskPort, SaveTaskPort saveTaskPort) {
+        return new EditTaskDescriptionUseCase(getTaskPort, saveTaskPort);
+    }
+
+    @Bean
+    UnassignTaskUseCase unassignTaskUseCase(GetTaskPort getTaskPort, SaveTaskPort saveTaskPort) {
+        return new UnassignTaskUseCase(getTaskPort, saveTaskPort);
+    }
+
+    @Bean
+    ViewUncompletedTasksUseCase viewUncompletedTasksUseCase(GetTasksPort getTasksPort) {
+        return new ViewUncompletedTasksUseCase(getTasksPort);
+    }
+}
